f39f002255c47e95196b9964b0a54d456d6c07f5
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-08-09 20:46:20 -0400

    started the player status table. card is being auto voted on - not good.

--- frontend_wtm/src/components/NavBar/PlayerStatus.jsx
+++ frontend_wtm/src/components/NavBar/PlayerStatus.jsx
@@ -1,20 +1,62 @@
-import Container from "react-bootstrap/esm/Container"
-import Row from "react-bootstrap/esm/Row"
+// import Table from 'react-bootstrap/Table'
 
-function PlayerStatus({players, playersThatVoted, selectedCards}) {
-    return (
-        <Container>
-            <Row>
-                <div>
-                    {players && players.map((card) => (
-                        // bootstrap table
-                        <h2> </h2>
-                    ))
-                    }
-                </div>
-            </Row>
-        </Container>
-    )
-}
+// function PlayerStatus({players, playersThatVoted, selectedCards}) {
+//     return (
+//                 <div>
+//                     {players
+//                     ?
+//                     <div>
+//                         <h2>Game: {gameCode}</h2>
+//                         <h2>Player Status</h2>
+//                         <Table striped size="sm">
+//                         <thead>
+//                             <tr>
+//                             <th></th>
+//                             {players && players.map((player) => (
+//                                 <th>{player.email}</th>
+//                             ))
+//                             }
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//                             <tr>
+//                             <td>Selected a Card?</td>
+//                             {players && players.map((player) => (
+//                                 // logic for if the player has selected a card. "Y" if they have "N" if they havent
+//                                 // maybe separate function/component?
+//                                 <td>{player.___}</td>
+//                             ))
+//                             }
+//                             </tr>
+//                             <tr>
+//                             <td>Voted?</td>
+//                             {players && players.map((player) => (
+//                                 //sort players that have voted and players to make sure they line up. then yes or no
+//                                 <td>{player.email}</td>
+//                             ))
+//                             }
+//                             </tr>
+//                             <tr>
+//                             {players && players.map((player) => (
+//                                 <td>{player.points}</td>
+//                             ))
+//                             }
+//                             </tr>
+//                         </tbody>
+//                         </Table>
+//                     </div>
+//                     :
+//                     ""
 
-export default PlayerStatus
+//                     }
+//                 </div>
+//     )
+// }
+
+// export default PlayerStatus
+
+// //                                         Player Status
+// //                         player1         player 2
+// // selected card?              Y
+// // voted?                      N
+// // points?                     2
