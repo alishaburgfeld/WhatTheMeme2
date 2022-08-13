92f0af422df2f6fde266a15c792de270aa36d9e7
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-08-13 12:33:10 -0400

    just moved a couple of files around

--- frontend_wtm/src/pages/HomePage.jsx
+++ frontend_wtm/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import {useEffect} from 'react'
-import LoggedInMapper from '../components/LoggedInMapper';
-import NoUserMapper from '../components/NoUserMapper';
-import {OffCanvas} from '../components/OffCanvas';
+import LoggedInMapper from '../components/HomePage/LoggedInMapper';
+import NoUserMapper from '../components/HomePage/NoUserMapper';
+import {OffCanvas} from '../components/HomePage/OffCanvas';
 
 function HomePage ({whoAmI, user, hand, setHand, show, setShow, startGame, game, setGame}){
 
